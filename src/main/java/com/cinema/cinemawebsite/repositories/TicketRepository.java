d841c0541e93bafcb284d83b769504c5b1a36592
ImperTator644 <ukaczyski6@gmail.com>
2023-01-09 21:12:12 +0100

    Ticket reservation

--- src/main/java/com/cinema/cinemawebsite/repositories/TicketRepository.java
+++ src/main/java/com/cinema/cinemawebsite/repositories/TicketRepository.java
@@ -7,6 +7,7 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.jpa.repository.query.Procedure;
 
 import java.sql.Date;
+import java.util.List;
 
 public interface TicketRepository extends JpaRepository<Ticket, TicketId> {
     @Procedure
@@ -20,5 +21,10 @@ public interface TicketRepository extends JpaRepository<Ticket, TicketId> {
                        boolean discount);
 
     @Query(value = "SELECT isReserved(?1, ?2, ?3, ?4, ?5, ?6)", nativeQuery = true)
-    boolean isReserved(String title, Date date, Integer hour, Integer minute, Integer rowNumber, Integer seatNumber);
+    boolean isReservedSeat(String title, Date date, Integer hour, Integer minute, Integer rowNumber, Integer seatNumber);
+
+    @Query(value = "SELECT r.id_seat FROM reserved r " +
+            "WHERE r.id_ishow=?1 AND r.id_seat IN " +
+            "(SELECT s.id_seat FROM seat s WHERE s.id_cinema=?2 AND s.room_number=?3)", nativeQuery = true)
+    List<Integer> isReservedSeat(Integer idIShow, Integer idCinema, Integer roomNumber);
 }
--- src/main/java/com/cinema/cinemawebsite/service/ReserveService.java
+++ src/main/java/com/cinema/cinemawebsite/service/ReserveService.java
@@ -1,28 +1,37 @@
 package com.cinema.cinemawebsite.service;
 
+import com.cinema.cinemawebsite.entities.IShow;
+import com.cinema.cinemawebsite.model.dto.ReservationInformationDto;
 import com.cinema.cinemawebsite.model.dto.TicketDto;
 import com.cinema.cinemawebsite.repositories.TicketRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.sql.Date;
+import java.util.List;
 
 @Service
 public class ReserveService {
     @Autowired
     private TicketRepository ticketRepository;
 
-    public boolean reserveTicket(TicketDto ticketDto){
-        Date date = Date.valueOf(ticketDto.getDate());
-        if(isTicketReserved(ticketDto)){
+    public boolean reserveTicket(TicketDto ticketDto, ReservationInformationDto reservationInformationDto){
+        if(reservedSeats(ticketDto, reservationInformationDto)){
             return false;
         }
-        ticketRepository.reserveTicket(ticketDto.getEmail(), ticketDto.getTitle(), date, ticketDto.getHour(), ticketDto.getMinute(), ticketDto.getRow(), ticketDto.getSeat(), ticketDto.isDiscount());
+        ticketRepository.reserveTicket(reservationInformationDto.getEmail(), reservationInformationDto.getTitle(), reservationInformationDto.getDate(), reservationInformationDto.getHour(), reservationInformationDto.getMinute(), ticketDto.getRow(), ticketDto.getSeat(), ticketDto.isDiscount());
         return true;
     }
 
-    public boolean isTicketReserved(TicketDto ticketDto){
-        Date date = Date.valueOf(ticketDto.getDate());
-        return ticketRepository.isReserved(ticketDto.getTitle(), date, ticketDto.getHour(), ticketDto.getMinute(), ticketDto.getRow(), ticketDto.getSeat());
+    public boolean reservedSeats(TicketDto ticketDto, ReservationInformationDto reservationInformationDto){
+        return ticketRepository.isReservedSeat(reservationInformationDto.getTitle(),
+                reservationInformationDto.getDate(),
+                reservationInformationDto.getHour(),
+                reservationInformationDto.getMinute(),
+                ticketDto.getRow(),
+                ticketDto.getSeat());
+    }
+
+    public List<Integer> reservedSeats(IShow iShow){
+        return ticketRepository.isReservedSeat(iShow.getIdShow(), iShow.getIdCinema(), iShow.getRoomNumber());
     }
 }
